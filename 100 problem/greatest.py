2beb75c29b04b4d5caf721921d6ce6b37eee2e28
SURYA <vsurya2107@gmail.com>
2026-07-14 20:32:05 +0530

    Add files via upload

---
+++ 100 problem/greatest.py
@@ -0,0 +1,9 @@
+a=int(input("enter your 1st numer"))
+b=int(input("enter your 2nd numer"))
+
+if(a>b):
+    print("1st number is greater")
+elif(b>a):
+    print("2nd number is greater")
+elif(a==b):
+    print("both are equal")
---
+++ 100 problem/n.natural.py
@@ -0,0 +1,7 @@
+a = [2,4,5,6]
+sum =0
+
+for i in a:
+    sum = sum + i 
+
+    print (sum)
