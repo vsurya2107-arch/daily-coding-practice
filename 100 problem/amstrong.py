ec8f8de7330a819fad64b856670a14e04ee2ae8a
SURYA <vsurya2107@gmail.com>
2026-07-18 14:52:12 +0530

    Update amstrong.py

--- 100 problem/amstrong.py
+++ 100 problem/amstrong.py
@@ -1,1 +1,14 @@
+a = int (input (" enter you number"))
+temp = a 
+count = len (str(a))   # number of digit theriya 
+total = 0 # storing present value for each itration 
 
+while temp > 0:
+    lastnumber = temp%10
+    total = total+(lastnumber** count)
+    temp = temp//10
+
+if a == total :
+    print("its amstrong number",a)
+else:
+    print("its not amstrong number ",a)
